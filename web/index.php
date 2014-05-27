ba1a28036508c56cda323da68803a051b9ae2f22
miccarr <miccarr@iLuffy.local>
2014-05-27 18:00:45 +0200

    test for php 5.5

--- web/index.php
+++ web/index.php
@@ -5,6 +5,11 @@
 		Do what the fuck you want with this, just keep this comment intact ;-)
 	*/
 	
+	$version = explode('.', phpversion());
+	$version = $version[0] * 10000 + $version[1] * 100 + $version[2];
+	if($version<50500)
+		die('Need PHP > 5.5');
+	unset($version);
 
 	// Loading the WHEEL core
 	include('../core/easyFunctions.php');
@@ -18,12 +23,6 @@
 	include('../core/session.php');
 	include('../core/helper.php');
 	include('../core/render.php');
-
-	$version = explode('.', phpversion());
-	$version = $version[0] * 10000 + $version[1] * 100 + $version[2];
-	if($version<50500)
-		die('PHP 5.5 Minimum !');
-	unset($version);
 
 	$_['stdOut'] = wheel_Router::route( $_SERVER['REQUEST_URI'] );
 	ob_end_clean();
